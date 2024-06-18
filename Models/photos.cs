c17ab3419ce054b8dede3904fbc250aa7ee6e43b
ana vitoria e carlos tevez <anatevezvitoriarosseto@gmail.com>
2024-06-18 12:29:52 -0300

    Atividade avaliativa Http Photos

---
+++ Models/photos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividadeav2.Models
+{
+    public class photos
+    {
+        public string AlbumId { get; set; }
+        public int id { get; set; }
+        public int Id { get => id; set => id = value; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ Services/RestService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Atividadeav2.Models;
+
+namespace Atividadeav2.Services
+{
+    internal class RestService
+    {
+        private HttpClient client;
+        private photos photos;
+        private List<photos> Photos;
+        private JsonSerializerOptions _serializerOptions;
+
+        RestService()
+        {
+            client = new HttpClient();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+        }
+        public async Task<List<photos>> GetPhotosAsync()
+        {
+            Uri uri = new Uri("https://jsonplaceholder.typicode.com/photos");
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Photos = JsonSerializer.Deserialize<List<photos>>(content, _serializerOptions);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            return Photos;
+        }
+    }
+}
+
+
